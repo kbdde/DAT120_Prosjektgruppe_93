3010c154717fda39699ffe76ce72961ab6592013
ErlendKK <136587757+ErlendKK@users.noreply.github.com>
2023-09-29 13:18:12 +0200

    fjernet tomme kommentarer

--- deloppgave_m.py
+++ deloppgave_m.py
@@ -1,11 +1,6 @@
-# 271408
-# deloppgave m
-
 from deloppgave_f import lengste_nullsekvens
 from lister_for_del_1 import dogn_nedbor
 
 talliste = dogn_nedbor
 lengde = lengste_nullsekvens(dogn_nedbor)
 print(f"Lengden på den lengste sammenhengende nullsekvensen er: {lengde}")
-
-#kommentar
